98f2356af0368eb972c15a89f1ed48a470e0bb70
Samuel-dot-cloud <successam336@gmail.com>
2020-12-30 21:10:26 +0300

    Initial project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCO7_z13YPh-FaFC2YuBIkuHo8p-oQ0ppw",
+    authDomain: "kanban-board-75d43.firebaseapp.com",
+    projectId: "kanban-board-75d43",
+    storageBucket: "kanban-board-75d43.appspot.com",
+    messagingSenderId: "222887479762",
+    appId: "1:222887479762:web:3ae19ce6ea9f2526ff600b",
+    measurementId: "G-QTM9F4Y3B5"
+  }
 };
 
 /*
